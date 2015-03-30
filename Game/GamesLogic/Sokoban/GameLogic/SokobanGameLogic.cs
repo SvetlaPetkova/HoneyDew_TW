d4099b7780a7097813f6031a9a57f27cf71fa0d7
ni4ka7a <n.atanasow@yahoo.com>
2015-03-30 22:31:51 +0300

    Updating objects in the Sokoban game to be readable form the file objects.txt in main directory

--- Game/GamesLogic/Sokoban/GameLogic/SokobanGameLogic.cs
+++ Game/GamesLogic/Sokoban/GameLogic/SokobanGameLogic.cs
@@ -26,75 +26,47 @@ namespace Game.GamesLogic.Sokoban.GameLogic
         private IEnumerable<IGameObject> FillWithBlocks()
         {
             var objs = new List<IGameObject>();
-            objs.Add(new Block(new Position(14, 10)));
-            objs.Add(new Block(new Position(19, 10)));
-            objs.Add(new Block(new Position(24, 10)));
-            objs.Add(new Block(new Position(14, 18)));
-            objs.Add(new Block(new Position(19, 18)));
-            objs.Add(new Block(new Position(24, 18)));
-            objs.Add(new Elevator(new Position(17, 5)));
-            objs.Add(new Elevator(new Position(20, 3)));
-            objs.Add(new Elevator(new Position(22, 5)));
-            objs.Add(new Elevator(new Position(17, 23)));
-            objs.Add(new Elevator(new Position(20, 25)));
-            objs.Add(new Elevator(new Position(22, 23)));
 
-            //drowing the walls
+            File inputFile = new File(@"..\..\..\objects.txt");
+            var separateLines = inputFile.ReadFile().ToString().Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
-            objs.Add(new Wall(new Position(0, 1), true, 10));
-            objs.Add(new Wall(new Position(10, 0), true, 20));
-            objs.Add(new Wall(new Position(30, 1), true, 11));
-            objs.Add(new Wall(new Position(0, 1), false, 10));
-            objs.Add(new Wall(new Position(40, 1), false, 10));
-            objs.Add(new Wall(new Position(0, 10), true, 6));
-            objs.Add(new Wall(new Position(34, 10), true, 6));
-            objs.Add(new Wall(new Position(6, 10), false, 10));
-            objs.Add(new Wall(new Position(34, 10), false, 10));
-            objs.Add(new Wall(new Position(0, 20), true, 7));
-            objs.Add(new Wall(new Position(34, 20), true, 6));
-            objs.Add(new Wall(new Position(0, 20), false, 10));
-            objs.Add(new Wall(new Position(40, 20), false, 10));
-            objs.Add(new Wall(new Position(0, 30), true, 10));
-            objs.Add(new Wall(new Position(10, 31), true, 20));
-            objs.Add(new Wall(new Position(30, 30), true, 11));
-            objs.Add(new Wall(new Position(13, 14), true, 14));
-            objs.Add(new Wall(new Position(13, 15), true, 14));
-            objs.Add(new Wall(new Position(4, 5), true, 6));
-            objs.Add(new Wall(new Position(4, 6), true, 6));
-            objs.Add(new Wall(new Position(31, 5), true, 6));
-            objs.Add(new Wall(new Position(31, 6), true, 6));
-            objs.Add(new Wall(new Position(14, 5), true, 3));
-            objs.Add(new Wall(new Position(14, 6), true, 3));
-            objs.Add(new Wall(new Position(19, 5), true, 3));
-            objs.Add(new Wall(new Position(19, 6), true, 3));
-            objs.Add(new Wall(new Position(24, 5), true, 3));
-            objs.Add(new Wall(new Position(24, 6), true, 3));
-            objs.Add(new Wall(new Position(11, 10), true, 3));
-            objs.Add(new Wall(new Position(11, 11), true, 3));
-            objs.Add(new Wall(new Position(16, 10), true, 3));
-            objs.Add(new Wall(new Position(16, 11), true, 3));
-            objs.Add(new Wall(new Position(21, 10), true, 3));
-            objs.Add(new Wall(new Position(21, 11), true, 3));
-            objs.Add(new Wall(new Position(26, 10), true, 3));
-            objs.Add(new Wall(new Position(26, 11), true, 3));
-            objs.Add(new Wall(new Position(11, 18), true, 3));
-            objs.Add(new Wall(new Position(11, 19), true, 3));
-            objs.Add(new Wall(new Position(16, 18), true, 3));
-            objs.Add(new Wall(new Position(16, 19), true, 3));
-            objs.Add(new Wall(new Position(21, 18), true, 3));
-            objs.Add(new Wall(new Position(21, 19), true, 3));
-            objs.Add(new Wall(new Position(26, 18), true, 3));
-            objs.Add(new Wall(new Position(26, 19), true, 3));
-            objs.Add(new Wall(new Position(4, 23), true, 6));
-            objs.Add(new Wall(new Position(4, 24), true, 6));
-            objs.Add(new Wall(new Position(31, 23), true, 6));
-            objs.Add(new Wall(new Position(31, 24), true, 6));
-            objs.Add(new Wall(new Position(14, 23), true, 3));
-            objs.Add(new Wall(new Position(14, 24), true, 3));
-            objs.Add(new Wall(new Position(19, 23), true, 3));
-            objs.Add(new Wall(new Position(19, 24), true, 3));
-            objs.Add(new Wall(new Position(24, 23), true, 3));
-            objs.Add(new Wall(new Position(24, 24), true, 3));
+            bool inWalls = false;
+            bool inBlocks = false;
+            bool inElevators = false;
+            string currentObject = string.Empty;
+
+            for (int i = 0; i < separateLines.Length; i++)
+            {
+                switch (separateLines[i])
+                {
+                    case "Walls\r": inWalls = true; inBlocks = false; inElevators = false; continue;
+                    case "Blocks\r": inWalls = false; inBlocks = true; inElevators = false; continue;
+                    case "Elevators\r": inWalls = false; inBlocks = false; inElevators = true; continue;
+
+                    default:
+                        break;
+                }
+
+                if (inWalls)
+                {
+                    var separateElements = separateLines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    objs.Add(new Wall(new Position(int.Parse(separateElements[0]), int.Parse(separateElements[1]))
+                        , separateElements[2] == "true" ? true : false, int.Parse(separateElements[3])));
+                }
+
+                if (inBlocks)
+                {
+                    var separateElements = separateLines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    objs.Add(new Block(new Position(int.Parse(separateElements[0]), int.Parse(separateElements[1]))));
+                }
+
+                if (inElevators)
+                {
+                    var separateElements = separateLines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    objs.Add(new Elevator(new Position(int.Parse(separateElements[0]), int.Parse(separateElements[1]))));
+                }
+            }
+            
 
             objs.Add(this.character);
             return objs;
